4962d7a252ce496b7101f6912cdb42a411a80132
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-08-23 10:21:07 -0500

    First checkin
    After extraction from QuiNovas/terraform-modules

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "endpoint" {
+  description = "The connection endpoint in address:port format."
+  value       = "${aws_db_instance.rds.endpoint}"
+}
+
+output "master_password" {
+  description = "The random master password assigned to the database."
+  sensitive   = true
+  value       = "${random_string.master_password.result}"
+}
+
+output "security_group_id" {
+  description = "The ID of the database security group."
+  value       = "${aws_security_group.rds.id}"
+}
